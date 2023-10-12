3b3a0285dd5a00c3e1f024a0aa189e1f926beab6
lucas araujo <lucas.trabalho.araujo@gmail.com>
2023-10-12 16:59:39 -0300

    Updated test fixtures and added cart requirement for order creation in orders_controller_test.rb

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -10,17 +10,26 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "requires item in cart" do
+    get new_order_url
+    assert_redirected_to store_index_url
+    assert_equal flash[:notice], "Your cart is empty"
+  end
+
   test "should get new" do
+    post line_items_url, params: { product_id: products(:ruby).id }
+
     get new_order_url
     assert_response :success
   end
 
   test "should create order" do
     assert_difference("Order.count") do
-      post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
+      post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name,
+                                          pay_type: @order.pay_type } }
     end
 
-    assert_redirected_to order_url(Order.last)
+    assert_redirected_to store_index_url
   end
 
   test "should show order" do
